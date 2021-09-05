a1eb5613b8b851b7aaf87eb35a4e3af6adc2ab2a
rockstream <88263242+rockstream@users.noreply.github.com>
2021-09-05 12:25:35 +0530

    Update Analytics.js

--- components/metrics/Analytics.js
+++ components/metrics/Analytics.js
@@ -7,7 +7,7 @@ export default function Analytics() {
   const { data } = useSWR('/api/views', fetcher);
 
   const pageViews = new Number(data?.total);
-  const link = 'https://leerob.io';
+  const link = 'https://tutordoc.vercel.app/';
 
   return <MetricCard header="All-Time Views" link={link} metric={pageViews} />;
 }
